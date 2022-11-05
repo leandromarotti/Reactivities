83877b12e1f9a6578fe7b42f95a643cfcdf1e2c2
leandromarotti <leandromarotti@gmail.com>
2022-11-04 21:51:07 -0400

    End of Section 8

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import React, { SyntheticEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 
@@ -33,9 +34,10 @@ export default observer(function ActivityList() {
 							</Item.Description>
 							<Item.Extra>
 								<Button
-									onClick={() => activityStore.selectActivity(activity.id)}
+									as={Link}
+									to={`/activities/${activity.id}`}
 									floated='right'
-									content='view'
+									content='View'
 									color='blue'
 								/>
 								<Button
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,19 +1,25 @@
 import { observer } from 'mobx-react-lite';
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { Button, Form, Segment } from 'semantic-ui-react';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
+import { v4 as uuid } from 'uuid';
 
 export default observer(function ActivityForm() {
+	const history = useHistory();
 	const { activityStore } = useStore();
 	const {
-		selectedActivity,
-		closeForm,
 		createActivity,
 		updateActivity,
-		loading
+		loading,
+		loadActivity,
+		loadingInitial
 	} = activityStore;
 
-	const intialState = selectedActivity ?? {
+	const { id } = useParams<{ id: string }>();
+
+	const [activity, setActivity] = useState({
 		id: '',
 		title: '',
 		category: '',
@@ -21,12 +27,26 @@ export default observer(function ActivityForm() {
 		date: '',
 		city: '',
 		venue: ''
-	};
+	});
 
-	const [activity, setActivity] = useState(intialState);
+	useEffect(() => {
+		if (id) loadActivity(id).then(activity => setActivity(activity!));
+	}, [id, loadActivity]);
 
 	function submitHandler() {
-		activity.id ? updateActivity(activity) : createActivity(activity);
+		if (activity.id.length === 0) {
+			let newActivity = {
+				...activity,
+				id: uuid()
+			};
+			createActivity(newActivity).then(() =>
+				history.push(`/activities/${newActivity.id}`)
+			);
+		} else {
+			updateActivity(activity).then(() =>
+				history.push(`/activities/${activity.id}`)
+			);
+		}
 	}
 
 	function inputChangeHandler(
@@ -35,6 +55,8 @@ export default observer(function ActivityForm() {
 		const { name, value } = event.target;
 		setActivity({ ...activity, [name]: value });
 	}
+
+	if (loadingInitial) return <LoadingComponent content='Loading activity...' />;
 
 	return (
 		<Segment clearing>
@@ -84,7 +106,8 @@ export default observer(function ActivityForm() {
 					content='Submit'
 				/>
 				<Button
-					onClick={closeForm}
+					as={Link}
+					to={`/activities/${activity.id}`}
 					floated='right'
 					type='button'
 					content='Cancel'
